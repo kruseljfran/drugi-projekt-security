522fa93eb71b63e2c66e33a6a8d2c0693f09bb75
kruseljfran <fk55025@fer.hr>
2025-11-09 00:43:08 +0100

    fixing local host hardcoding

--- src/app.ts
+++ src/app.ts
@@ -173,5 +173,6 @@ app.get("/attacker", (req: Request, res: Response) => {
 
 const port = parseInt(process.env.PORT || "5000");
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port} (port ${port})`);
+  console.log(`Listening on https://drugi-projekt-security.onrender.com
+`);
 });
